427d1069d68589746fffdf17e7a4ad98dd4598a4
mniudanri <niudanri12@gmail.com>
2026-04-28 06:54:57 +0700

    add missing type interface promo

--- src/data/promos.ts
+++ src/data/promos.ts
@@ -1,4 +1,6 @@
-export const promos = [
+import type { Promo } from "../models/Promo";
+
+export const promos : Promo[] = [
     { id: 1, title: "Spring Sale", text: "Up to 30% off shoes!", image: "/promos/promo1.png" },
     { id: 2, title: "Electronics Deal", text: "Wireless mouse only $19.99!", image: "/promos/promo2.png" },
     { id: 3, title: "Kitchen Essentials", text: "Buy 2 mugs, get 1 free!", image: "/promos/promo3.png" },
