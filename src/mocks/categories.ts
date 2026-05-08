f8fdec216d6ada4e1ed7c4b2a34f1ee4efe74073
mniudanri <niudanri12@gmail.com>
2026-05-08 20:35:20 +0700

    add dummy images and mock products, categories

---
+++ src/mocks/categories.ts
@@ -0,0 +1,10 @@
+export const categories = [
+    "All Categories",
+    "New Arrivals",
+    "Women",
+    "Men",
+    "Electronics",
+    "Home & Living",
+    "Beauty",
+    "Sports",
+  ];
---
+++ src/mocks/products.ts
@@ -0,0 +1,58 @@
+export const products = [
+    {
+      id: 1,
+      title: "Woman Bag",
+      price: "$69.99",
+      category: "Women",
+      image:
+        "/products/bag.png",
+    },
+    {
+      id: 2,
+      title: "Headset",
+      price: "$100.99",
+      category: "Electronics",
+      image:
+        "/products/headset.png",
+    },
+    {
+      id: 3,
+      title: "Smart Watch",
+      price: "$129.99",
+      category: "Electronics",
+      image:
+        "/products/smart-watch.png",
+    },
+    {
+      id: 4,
+      title: "Wireless Headphones",
+      price: "$99.99",
+      category: "Electronics",
+      image:
+        "/products/head-phone.png",
+    },
+    {
+      id: 5,
+      title: "Socks",
+      price: "$3.99",
+      category: "Sports",
+      image:
+        "/products/socks.png",
+    },
+    {
+      id: 6,
+      title: "Tumbler",
+      price: "$99.99",
+      category: "Sports",
+      image:
+        "/products/tumbler.png",
+    },
+    {
+      id: 7,
+      title: "Modern Lamp",
+      price: "$49.99",
+      category: "Beauty",
+      image:
+        "/products/lamp.png",
+    }
+  ];
