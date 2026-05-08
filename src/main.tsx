c6d347a60dd788bfdb969c7a0eee41a6d1ca26b7
mniudanri <niudanri12@gmail.com>
2026-05-08 20:36:13 +0700

    add react mantine component

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import { MantineProvider } from "@mantine/core";
+import "@mantine/core/styles.css";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <MantineProvider defaultColorScheme="light">
+      <App />
+    </MantineProvider>
+  </React.StrictMode>
+);
